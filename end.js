16abf98004b0482ddf0b706d9bfa60e757ca4fe1
Sebastian Frampton <sebastian_frampton@hotmail.com>
2020-09-02 08:27:10 +0800

    feat fixed end page saving highscore to return to end page

--- end.js
+++ end.js
@@ -24,5 +24,5 @@ saveHighScore = (e) => {
   highScores.splice(5);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("/");
+  window.location.assign("/Quiz-App/end.html");
 };
